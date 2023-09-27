aa81853e050aaf1ec3da68dc5989ab5f1013ebde
Lee Jungmin <99460302+Mungjin01@users.noreply.github.com>
2023-09-28 00:44:01 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    const a = num_list.reduce((acc, cur) => acc*cur)
+    const b = num_list.reduce((acc, cur) => acc+cur)
+    
+    if(a<b*b) {
+        return 1
+    }else {return 0}
+}
