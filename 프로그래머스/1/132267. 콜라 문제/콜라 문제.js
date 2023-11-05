59cc16fe207bfb902cc318ff55dc22c63b284dea
Lee Jungmin <99460302+Mungjin01@users.noreply.github.com>
2023-11-05 12:06:19 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,13 @@
+function solution(a, b, n) {
+    let answer = 0;
+    let remain = n;
+    
+    while (true) {
+        if(a > remain) {
+            break;
+        }
+        answer += parseInt(remain/a)*b;
+        remain = parseInt(remain/a)*b + remain%a
+    }
+    return answer;
+}
